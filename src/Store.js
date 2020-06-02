3133e8cfa87594a03b48e572060029be3810fdaa
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-06-02 11:06:02 +0100

    initial commit

---
+++ src/Store.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import io from 'socket.io-client';
+
+export const CTX = React.createContext();
+
+const initState = {
+    general: [
+        {from: 'obinna', msg: 'hello'},
+        {from: 'ammy', msg: 'hello'},
+        {from: 'victor', msg: 'hello'},
+    ],
+    topic2: [
+        {from: 'chiwendu', msg: 'hello'},
+        {from: 'michael', msg: 'hello'},
+        {from: 'gold', msg: 'hello'},
+    ]
+}
+
+
+
+function reducer(state, action) {
+    const {from, msg, topic} = action.payload;
+    switch(action.type) {
+        case 'RECEIVE_MESSAGE':
+            return {
+             ...state,
+             [topic]: [
+                 ...state[topic],
+                 {from, msg}
+             ] 
+
+             
+            }
+            default:
+                return state
+    }
+}
+
+let socket;
+
+function sendChatAction(value) {
+    socket.emit('chat message', value);
+
+}
+
+export default function Store(props) {
+
+    const [allChats, dispatch] = React.useReducer(reducer, initState);
+
+    if (!socket) {
+        socket = io(':3001');
+        socket.on('chat message', function(msg){
+            dispatch({type:'RECEIVE_MESSAGE', payload: msg});
+        });
+    }
+    const user = 'obinna' + Math.random(100).toFixed(2)
+
+    return (
+        <CTX.Provider value={{allChats, sendChatAction, user}}>
+          {props.children}
+        </CTX.Provider>
+    )
+}
---
+++ src/server/index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('chat message', function(msg){
+        console.log('message:' + msg);
+        console.log('message: ' + JSON.stringify(msg));
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3001, function(){
+    // server
+    console.log('listening on *:3001');
+});
